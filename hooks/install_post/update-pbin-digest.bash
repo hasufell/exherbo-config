c55a765758b840a6bbcff8af678fb4cef262243e
Julian Ospald <hasufell@posteo.de>
2016-10-03 13:47:50 +0200

    Fix pbin digest hook

--- hooks/install_post/update-pbin-digest.bash
+++ hooks/install_post/update-pbin-digest.bash
@@ -5,7 +5,7 @@ source "${PALUDIS_EBUILD_DIR}/echo_functions.bash"
 _my_pbin_repo="hasufell-binhost"
 
 if [[ ${CAVE_PERFORM_CMDLINE_destination} == ${_my_pbin_repo} ]] ; then
-	_mypn=$(cave print-ids --matching "${TARGET}" -f '%c/%p')
+	_mypn=$(cave print-ids --matching "=${TARGET}" -f '%c/%p')
 	einfo "Updating ${_mypn} digest in ${_my_pbin_repo}"
 
 	cave digest ${_mypn} ${_my_pbin_repo}
